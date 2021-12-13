a611118e722179f96a18b80091d07aebf110edbb
Laura Trentino <laura@trentino.com.au>
2021-12-13 11:30:34 +0800

    cleaned ipynb file

--- test_transformation_functions.py
+++ test_transformation_functions.py
@@ -3,20 +3,25 @@ from transform_functions import *
 
 
 
-def convert_country_code():
+def test_convert_country_code():
 
     # ASSEMBLE 
-    input_df = pd.main_df({
+    input_df = pd.DataFrame({
+        "id": [1,2],
         "country": ["Finland","Denmark"],
     })
 
-    expected_df = pd.main_df({
-        "code": ["FI","DK"], 
-    })
+    # expected_df = pd.main_df({
+    #     "id": [1,2],
+    #     "code": ["FI","DK"], 
+    # })
+    expected_result = ["FL","DK"]
 
     # ACT 
-    output_df = convert_country_code(input_df=input_df)
+    codes = convert_country_code(main_df=input_df)
 
     # ASSERT 
+    assert expected_result == codes
 
-    pd.testing.assert_frame_equal(left=output_df, right=expected_df,check_exact=True)
+    # JON SUGGESTED TO TEST BY RETURN A DATAFRAME
+    # pd.testing.assert_frame_equal(left=output_df, right=expected_df,check_exact=True)
